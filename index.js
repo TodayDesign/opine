c7fd3e8ed22fb380a38439b574edf341da5eca70
Tom McLean <tom.a.mclean@gmail.com>
2016-07-13 16:33:03 +1000

    :point_up:

---
+++ index.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var opine = require('gulp-opine');
+
+var concat = require('gulp-concat');
+
+var module = opine.module('vendor');
+
+var includes = module.getConfig('includes', []);
+var target = module.getConfig('target', 'vendor.js');
+var dest = opine.getDest('scripts');
+
+module.task(function() {
+    return gulp.src(includes)
+        .pipe(concat(target))
+        .pipe(gulp.dest(dest))
+        .pipe(module.size());
+});
