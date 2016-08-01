90186e20fe9af3012f6e01d5ebff6fcd87f95dc0
Giovanni Ferron <gvnnrulez@gmail.com>
2016-08-01 16:44:33 +1000

    Replace css-nano plugin with clean-css. Added mqpacker and sourcemaps

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ var opine = require('gulp-opine');
 
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
-var cssnano = require('gulp-cssnano');
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('autoprefixer');
-
+var mqpacker = require('css-mqpacker');
+var sourcemaps = require('gulp-sourcemaps');
 var module = opine.module('styles');
 var sources = module.getSources();
 var dest = module.getDest();
 var includes = module.getConfig('includes', []);
 
+var debug = module.getConfig('debug', true);
+
 module.addBuild();
 module.addWatch(sources);
 
 module.task(function() {
+
     var sasspipe = sass({
             outputStyle: 'expanded',
             precision: 6,
@@ -22,13 +26,25 @@ module.task(function() {
         })
         .on('error', sass.logError);
 
-    return gulp.src(sources)
-        .pipe(sasspipe)
+    var b = gulp.src(sources)
+
+    if(debug) {
+        b = b.pipe(sourcemaps.init());
+    }
+
+    b = b.pipe(sasspipe)
         .pipe(postcss([
-            autoprefixer({ browsers: ['last 3 versions', '> 5%', 'IE >= 0']})
-        ]))
-        .pipe(cssnano())
+            autoprefixer({ browsers: ['last 3 versions', '> 5%', 'IE >= 0']}),
+            mqpacker()
+        ]));
+
+    if(!debug) {
+        b = b.pipe(cleanCSS({ compatibility: 'ie8' }))
+    } else {
+        b = b.pipe(sourcemaps.write());
+    }
+
+    return b
         .pipe(gulp.dest(dest))
         .pipe(module.size());
 });
-
