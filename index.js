3af058e08c2b9e5cad60589288eadee36aa00437
Giovanni Ferron <gvnnrulez@gmail.com>
2016-08-09 16:11:32 +1000

    Remove source maps and combine media queries

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('autoprefixer');
-var mqpacker = require('css-mqpacker');
-var sourcemaps = require('gulp-sourcemaps');
 var module = opine.module('styles');
+
 var sources = module.getSources();
 var dest = module.getDest();
 var includes = module.getConfig('includes', []);
@@ -21,27 +20,20 @@ module.task(function() {
 
     var sasspipe = sass({
             outputStyle: 'expanded',
-            precision: 6,
+            precision: 7,
             includePaths: includes
         })
         .on('error', sass.logError);
 
     var b = gulp.src(sources)
 
-    if(debug) {
-        b = b.pipe(sourcemaps.init());
-    }
-
     b = b.pipe(sasspipe)
         .pipe(postcss([
-            autoprefixer({ browsers: ['last 3 versions', '> 5%', 'IE >= 0']}),
-            mqpacker()
+            autoprefixer({ browsers: ['last 3 versions', '> 5%', 'IE >= 0']})
         ]));
 
     if(!debug) {
         b = b.pipe(cleanCSS({ compatibility: 'ie8' }))
-    } else {
-        b = b.pipe(sourcemaps.write());
     }
 
     return b
