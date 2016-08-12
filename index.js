a5b551a0cf754461a6863787588fd22baa79cae7
Tom McLean <tom.a.mclean@gmail.com>
2016-08-12 11:12:06 +1000

    Add task to alt-watch

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var module = opine.module('serve');
 var port = module.getConfig('port', 8000);
 var dir = module.getConfig('root', opine.getDest(''));
 
+module.addAltWatch();
+
 module.task(function(done) {
     var serve = serveStatic(dir);
     http.createServer(function(req, res) {
