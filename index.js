7b8711370785bf10ef51ba62c673b446d665da81
Tom McLean <tom.a.mclean@gmail.com>
2016-08-12 10:11:58 +1000

    :point_up:

---
+++ index.js
@@ -0,0 +1,19 @@
+var opine = require('gulp-opine');
+
+var http = require('http');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var module = opine.module('serve');
+
+var port = module.getConfig('port', 8000);
+var dir = module.getConfig('root', opine.getDest(''));
+
+module.task(function(done) {
+    var serve = serveStatic(dir);
+    http.createServer(function(req, res) {
+        var done = finalhandler(req, res);
+        serve(req, res, done);
+    }).listen(port);
+});
+
