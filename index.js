3a6e623749294e17127865f98d83e24df3d866f3
Tom McLean <tom.a.mclean@gmail.com>
2016-08-31 11:58:29 +1000

    :point_up:

---
+++ index.js
@@ -0,0 +1,39 @@
+var opine = require('gulp-opine');
+var gulp = require('gulp');
+
+var spawn = require('child_process').spawn;
+
+var module = opine.module('silverstripe-build');
+
+var host = opine.getConfig('rsync.host');
+var user = opine.getConfig('rsync.user', 'deploy');
+var cwd = opine.getConfig('rsync.dest', '/var/www/html');
+
+module.task(function(done) {
+    var conn;
+    if(user) {
+        conn = user + '@' + host;
+    } else {
+        conn = host;
+    }
+
+    var args = [conn,
+        'cd', cwd, ';',
+        'php', 'framework/cli-script.php', '/dev/build'
+        ];
+    console.log(args);
+    var ssh = spawn('ssh', args);
+
+    ssh.stdout.on('data', function(data) {
+        console.log(''+data);
+    });
+
+    ssh.stderr.on('data', function(data) {
+        console.error(''+data);
+    });
+
+    ssh.on('close', function(code) {
+        console.log('SSH complete.');
+        done();
+    });
+});
