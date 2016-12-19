073cc1baa4a39d22cacd8e90f284c234d3a4ef21
Tom McLean <tom.a.mclean@gmail.com>
2016-12-19 16:25:34 +1100

    Mention listened port in output

--- opine-serve/index.js
+++ opine-serve/index.js
@@ -13,6 +13,7 @@ module.addAltWatch();
 
 module.task(function(done) {
     var serve = serveStatic(dir);
+    console.log('Serving', dir, 'on port', port, '...');
     http.createServer(function(req, res) {
         var done = finalhandler(req, res);
         serve(req, res, done);
